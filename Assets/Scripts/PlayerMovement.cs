ff346d63e34dffa35c422bff8e01b38f8a71b891
4rleco <117652001+4rleco@users.noreply.github.com>
2026-03-30 13:08:20 -0300

    Add physics to the project

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,21 +1,33 @@
 using UnityEngine;
 using UnityEngine.UIElements;
 
+[RequireComponent(typeof(Rigidbody))]
+
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float speed;
     [SerializeField] private Engine leftEngine;
     [SerializeField] private Engine rightEngine;
+    [SerializeField] private float force;
+    [SerializeField] private float toqrueForce;
+    [SerializeField] private ForceMode forceMode;
 
-    private void Update()
+    private Rigidbody rigidbody;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
     {
         //          INPUT
         Vector3 movement = Vector3.zero;
 
+        float rotateZ = 0;
+
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
         movement.z = 0;
-        float roatateZ = 0;
 
         if (Input.GetKey(KeyCode.Space))
             movement.z = -1;
@@ -24,17 +36,25 @@ public class PlayerMovement : MonoBehaviour
             movement.z = 1;
 
         if (Input.GetKey(KeyCode.Q))
-            roatateZ = 1;
+            rotateZ = 1;
 
         if (Input.GetKey(KeyCode.E))
-            roatateZ = -1;
+            rotateZ = -1;
 
         //          MOVEMENT
-        transform.Rotate(0, 0, roatateZ);
-        transform.Translate(movement * (speed * Time.deltaTime));
+        rigidbody.AddTorque(Vector3.up * (rotateZ * force), forceMode);
+        rigidbody.AddForce(movement * force, forceMode);
 
         //          SFX
         leftEngine.Set(movement.x > 0);
         rightEngine.Set(movement.x < 0);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Planet"))
+        {
+            Debug.Log($"Collision with {collision.gameObject.name}");
+        }
     }
 }
